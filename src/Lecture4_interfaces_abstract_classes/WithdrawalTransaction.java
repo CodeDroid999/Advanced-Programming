11b340b9682a8ddb6f2dcabfb1b246cdb0d2c75b
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2024-11-28 09:50:54 +0300

    Update WithdrawalTransaction.java

--- src/Lecture4_interfaces_abstract_classes/WithdrawalTransaction.java
+++ src/Lecture4_interfaces_abstract_classes/WithdrawalTransaction.java
@@ -5,41 +5,48 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Calendar;
 
 public class WithdrawalTransaction extends BaseTransaction {
+
     public WithdrawalTransaction(int amount, @NotNull Calendar date) {
         super(amount, date);
     }
 
-    private boolean checkDepositAmount(int amt) {
-        if (amt < 0) {
-            return false;
-        } else {
-            return true;
-        }
+    /**
+     * Checks if the transaction amount is valid (greater than 0).
+     * @param amt the amount to check
+     * @return true if the amount is valid, false if negative
+     */
+    private boolean checkTransactionAmount(int amt) {
+        return amt >= 0;
     }
 
-    // Method to reverse the transaction
+    /**
+     * Reverses the withdrawal transaction (adds the amount back to the account).
+     * @return true if the reversal was successful
+     */
     public boolean reverse() {
-        return true;
-    } // return true if reversal was successful
-
-    // Method to print a transaction receipt or details
-    public void printTransactionDetails() {
-        System.out.println("Deposit Trasaction: " + this.toString());
+        // A simple reversal: Credit the withdrawal amount back to the account
+        // The reversal process might include additional checks (e.g., transaction history, permissions, etc.)
+        return true;  // Assuming reversal is always successful here (this can be extended)
     }
 
-    /*
-    Oportunity for assignment: implementing different form of withdrawal
+    /**
+     * Prints the transaction details for the withdrawal.
+     */
+    public void printTransactionDetails() {
+        System.out.println("Withdrawal Transaction: " + this.toString());
+    }
+
+    /**
+     * Applies the withdrawal to the bank account by decreasing the balance.
+     * The withdrawal is only successful if the account has sufficient funds.
      */
     public void apply(BankAccount ba) {
         double curr_balance = ba.getBalance();
-        if (curr_balance > getAmount()) {
+        if (curr_balance >= getAmount()) {
             double new_balance = curr_balance - getAmount();
             ba.setBalance(new_balance);
+        } else {
+            System.out.println("Insufficient funds for withdrawal!");
         }
     }
-
-    /*
-    Assignment 1 Q3: Write the Reverse method - a method unique to the WithdrawalTransaction Class
-     */
 }
-
